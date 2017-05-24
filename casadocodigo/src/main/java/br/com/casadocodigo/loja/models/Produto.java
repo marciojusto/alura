7f90255eaa99bc105642991d10bef0264ea74670
Marcio Justo <marcio.justo@gmail.com>
2017-05-24 06:56:58 -0300

    Projeto Spring MVC com JPA da Alura

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,61 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String titulo;
+	private String descricao; 
+	private String paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getPaginas() {
+		return paginas;
+	}
+	
+	public void setPaginas(String paginas) {
+		this.paginas = paginas;
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+
+}
